a5a2e0c05be3c1808cd8e2536dded46ba03b7dbd
Raj Bandi <raj.bandi@hotmail.com>
2026-08-11 09:33:58 +1000

    Publish books from workflows repository

--- website/books/hbc-scrnaseq/html/searcher-c2a407aa.js
+++ website/books/hbc-scrnaseq/html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-970e72af.js',
+                path_to_root + 'searchindex-d4709f96.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
--- website/books/single-cell-rna-seq/html/searcher-c2a407aa.js
+++ website/books/single-cell-rna-seq/html/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fd0f491a.js',
+                path_to_root + 'searchindex-d258225a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
